00cb8874fe1acc3da1a1c3a52df637b7cd4cc046
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-08-03 16:32:47 +0530

    Reverse String
    Added new code which can be used for Reverse a String / word.

---
+++ Strings/ReverseString.java
@@ -0,0 +1,26 @@
+// Write a program to reverse the string 
+// Example: Input:
+//         s = Geeks
+//         Output: skeeG
+
+// package Strings;
+
+public class ReverseString{
+    public static String ReverseWord(String str){
+        int i = 0;
+        int j = str.length() - 1;
+        char[] ch = str.toCharArray();
+        while(i <= j){
+            char temp = ch[i];
+            ch[i] =  ch[j];
+            ch[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(ch);
+    }
+    public static void main(String[] args) {
+        String str = "Geeks";
+        System.out.println(ReverseWord(str));
+    }
+}
