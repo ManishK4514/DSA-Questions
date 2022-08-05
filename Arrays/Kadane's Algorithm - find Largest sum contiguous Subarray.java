81f9f42b5e7a243e97096247c8628410d714976e
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-08-05 19:12:16 +0530

    Kadane's Algorithm -Largest sum contiguous Subarrays

--- Arrays/Kadane's Algorithm - find Largest sum contiguous Subarray.java
+++ Arrays/Kadane's Algorithm - find Largest sum contiguous Subarray.java
@@ -11,7 +11,7 @@
 
 */
 public class findLargestSumContiguousSubarray {
-    public static void largestSum(int[] arr, int n) {
+    public static int largestSum(int[] arr, int n) {
         /*
          * BruteForce Approach
          * int max = Integer.MIN_VALUE;
@@ -28,29 +28,27 @@ public class findLargestSumContiguousSubarray {
          * ans = max;
          * }
          * }
-         * System.out.println(ans);
+         * return ans;
          */
 
         // Optimized Approach
-        int max = Integer.MIN_VALUE;
-        int ans = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    sum += arr[k];
-                }
-                if (sum > max) {
-                    max = sum;
-                }
-                ans = max;
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            currSum += arr[i];
+            if(maxSum < currSum){
+                maxSum = currSum;
+            }
+            if(currSum < 0){
+                currSum = 0;
             }
         }
-        System.out.println(ans);
+        return maxSum;
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, -2, 5 };
-        largestSum(arr, arr.length);
+        int ans = largestSum(arr, arr.length);
+        System.out.println(ans);
     }
 }
