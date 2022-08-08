2658dd19ecbaad65f72d7c4b785c0423501226b0
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-08-08 12:42:54 +0530

    Added Program to Sort an array by using Insertion sort.
    This is the Program to Sort an array by using Insertion sort.

--- Sorting Algorithm/Insertion Sort.java
+++ Sorting Algorithm/Insertion Sort.java
@@ -1,56 +1,24 @@
-public class mergeSort {
-    public static void merge(int arr[], int si, int mid, int ei) {
-        // creating a temp array.
-        int[] temp = new int[ei - si + 1];
-        int i = si;
-        int j = mid + 1;
-        int k = 0;
-        while(i <= mid && j <= ei){
-            if(arr[i] <= arr[j]){
-                temp[k] = arr[i];
-                i++;
+// Write a program to Sort an array by using insertion sort.
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr){
+        for(int i = 1; i < arr.length; i++){
+            int temp = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > temp){
+                arr[j + 1] = arr[j];
+                j--;
             }
-            else{
-                temp[k] = arr[j];
-                j++;
-            }
-            k++;
-        }
-        if(i > mid){
-            while(j <= ei){
-                temp[k] = arr[j];
-                j++; k++;
-            }
-        }
-        else{
-            while(i <= mid){
-                temp[k] = arr[i];
-                i++; k++;
-            }
-        }
-        // copying all the elements of temp in original array.
-        for (int m = 0, n = si; m < temp.length; m++, n++) {
-            arr[n] = temp[m];
+            // placement
+            arr[j + 1] = temp;
         }
     }
-
-    public static void divide(int[] arr, int si, int ei) {
-        if (si >= ei) {
-            return;
-        }
-        int mid = si + (ei - si) / 2;
-        // Dividing into sub-arrays.
-        divide(arr, si, mid);
-        divide(arr, mid + 1, ei);
-        merge(arr, si, mid, ei);
-    }
-
     public static void main(String[] args) {
-        int arr[] = { 41, 1, 32, 95, 7 };
-        divide(arr, 0, arr.length - 1);
+        int arr[] = { 4, 1, 3, 9, 7};
+        insertionSort(arr);
 
         // Printing the array
-        for (int values : arr) {
+        for(int values: arr){
             System.out.print(values + " ");
         }
     }
